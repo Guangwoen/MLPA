7912086ac34e8aa2fe4684166416ddc417cbe2b2
cuiguangyuan <glisten_cui@outlook.com>
2024-10-08 14:22:18 +0800

    add clustering part

---
+++ src/ClusteringBase.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 崔光远 on 2024/10/8.
+//
+
+#include "ClusteringBase.h"
+
+namespace mlpa::clst {
+
+
+} // namespace mlpa::clst
---
+++ src/ClusteringBase.h
@@ -0,0 +1,12 @@
+//
+// Created by 崔光远 on 2024/10/8.
+//
+
+#ifndef CLUSTERINGBASE_H
+#define CLUSTERINGBASE_H
+
+namespace mlpa::clst {
+    class ClusteringBase {};
+} // namespace mlpa::clst
+
+#endif //CLUSTERINGBASE_H
